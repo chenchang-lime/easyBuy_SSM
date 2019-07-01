44bfa0d8dca5a593641d777432a04ef7e814ba67
ASUS <ASUS@chenchang-laptop>
2019-07-01 09:02:17 +0800

    陈昌：1：根据类别查询商品；2：商品详情页；3：加入购物车，查看购物车，修改购买数量，移除出购物车。4：强制保留两位小数显示，时间格式显示工具JS。2019.7.1

--- src/main/webapp/jquery/util.js
+++ src/main/webapp/jquery/util.js
@@ -16,3 +16,15 @@ function saveDecimal2(x) {
 	}
 	return s; 
 } 
+//DATE时间格式转换为中文显示
+function timeToString(time){
+    var datetime = new Date();
+    datetime.setTime(time);
+    var year = datetime.getFullYear();
+    var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+    var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+    var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();
+    var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+    var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+    return year + "-" + month + "-" + date+" "+hour+":"+minute+":"+second;
+}
